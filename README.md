18eefa665ce085460384b44a71a9d3e1fe0c0d6e
matheusfgomes <matheusfgomes935@gmail.com>
2019-11-29 08:41:00 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-qrhj6h
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-qrhj6h)
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+procution: false,
+firebase: {
+  apiKey: "AIzaSyD7KuFa1iZ2VqP1C0o_nOSLFOKZXpc7OQU",
+    authDomain: "contato-84550.firebaseapp.com",
+    databaseURL: "https://contato-84550.firebaseio.com",
+    projectId: "contato-84550",
+    storageBucket: "contato-84550.appspot.com",
+    messagingSenderId: "765316494201",
+    appId: "1:765316494201:web:82ac786ad45ec8193af5a8"
+}
+};
